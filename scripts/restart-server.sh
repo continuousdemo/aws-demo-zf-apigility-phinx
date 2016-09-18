1dfa53cb0427a912e0db9ef0aafbadfcc8fa2c7b
Oswald De Riemaecker <oswald@continuousphp.com>
2016-09-18 20:46:23 +0200

    Fixing the server restart

--- scripts/restart-server.sh
+++ scripts/restart-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-service apache2 restart > /dev/null 2>&1 &
-service php5-fpm restart > /dev/null 2>&1 &
+/etc/init.d/apache2 restart > /dev/null 2>&1 
+/etc/init.d/php7.0-fpm restart > /dev/null 2>&1
